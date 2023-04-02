25338ccae38368def3fc3c1052794964ab1858fe
Hadia Jalil <hadiajalil2@gmail.com>
2023-04-02 18:07:06 +0200

    Initial Commit

---
+++ Posts/PostsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostsApp.swift
+//  Posts
+//
+//  Created by Hadia Jalil on 02/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct PostsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
